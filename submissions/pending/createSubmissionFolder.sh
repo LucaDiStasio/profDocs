9ff780b471bc644281540ab472772a79d559d7d1
Luca Di Stasio <luca.distasio@gmail.com>
2022-08-09 18:36:34 +0300

    commit on 2022-08-09 at 18:36:34

--- submissions/pending/createSubmissionFolder.sh
+++ submissions/pending/createSubmissionFolder.sh
@@ -1,1 +1,1 @@
-mkdir $1  && cp ../compl*/Solico/*CV.pdf $1  && cp ../../eng*/cover*/cover*template.tex $1  && ls $1 
+mkdir $1  && cp ../../english/resume/DiStasioLuca-CV.pdf $1  && cp ../../eng*/cover*/cover*template.tex $1  && ls $1 
